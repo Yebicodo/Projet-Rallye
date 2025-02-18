c03a56c5b1772fc013c4115dd149d9c8fb1ec1bc
Gence <vivierayan@gmail.com>
2025-02-18 14:59:56 +0400

    modif

--- Projet exam Rallye/Rallye/footer.php
+++ Projet exam Rallye/Rallye/footer.php
@@ -1,6 +1,14 @@
 <footer>
-        <p>&copy; 2024 RallyePéÏ. Tous droits réservés.</p>
-    </footer>
-    <script src="script.js"></script>
+    <p>&copy; 2024 RallyePéÏ. Tous droits réservés.</p>
+    <div class="footer-icons">
+        <a href="https://lsar.re" target="_blank">
+            <img src="site-internet.png" alt="LSAR">
+        </a>
+        <a href="https://www.facebook.com/PixelRallye/?locale=fr_FR" target="_blank">
+            <img src="facebook.png" alt="Facebook">
+        </a>
+    </div>
+</footer>
+<script src="script.js"></script>
 </body>
 </html>
