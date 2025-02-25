33a4a68835d6771fcdd7555e11540695ee9b1143
Gence <vivierayan@gmail.com>
2025-02-25 11:12:11 +0400

    modif du footer icone

--- Projet exam Rallye/Rallye/footer.php
+++ Projet exam Rallye/Rallye/footer.php
@@ -2,7 +2,7 @@
     <p>&copy; 2024 RallyePéÏ. Tous droits réservés.</p>
     <div class="footer-icons">
         <a href="https://lsar.re" target="_blank">
-            <img src="site-internet.png" alt="LSAR">
+            <img src="Logo-LSAR.png" alt="LSAR">
         </a>
         <a href="https://www.facebook.com/PixelRallye/?locale=fr_FR" target="_blank">
             <img src="facebook.png" alt="Facebook">
